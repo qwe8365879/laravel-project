54015b8e0ee9ddf58dd101ff30ffd8fa224d0701
qwe8365879 <jjj.jjjiang@gmail.com>
2019-09-05 11:47:38 +1000

    add product and pic tables

---
+++ database/migrations/2019_09_05_114511_create_pictures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('thumbnails');
+            $table->string('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
